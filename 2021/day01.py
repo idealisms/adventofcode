9fd9474389caf4d212d94cd09060594a4e6283fa
tony chang <tony@ponderer.org>
2021-12-01 20:49:32 -0800

    Clean up code for day 1.

--- 2021/day01.py
+++ 2021/day01.py
@@ -5,23 +5,18 @@ import re
 
 inp = open(re.match(r"day\d\d", __file__)[0] + 'input.txt').read().strip()
 
+nums = list(map(int, inp.split()))
+
 p = None
 part1 = 0
-for n in inp.split():
-    n = int(n)
-    if p is not None:
-        if n > p:
-            part1 += 1
-    p = n
+for i in range(len(nums) - 1):
+    if nums[i+1] > nums[i]:
+        part1 += 1
 print(part1)
 
 p = None
 part2 = 0
-nums = list(map(int, inp.split()))
 for i in range(len(inp) - 2):
-    n = sum(nums[i:i+3])
-    if p is not None:
-        if n > p:
-            part2 += 1
-    p = n
+    if sum(nums[i+1:i+4]) > sum(nums[i:i+3]):
+        part2 += 1
 print(part2)
