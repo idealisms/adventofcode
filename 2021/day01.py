a200e9236e24af600bab979110272cb443542135
tony chang <tony@ponderer.org>
2021-12-01 20:50:03 -0800

    Remove unused lines.

--- 2021/day01.py
+++ 2021/day01.py
@@ -7,14 +7,12 @@ inp = open(re.match(r"day\d\d", __file__)[0] + 'input.txt').read().strip()
 
 nums = list(map(int, inp.split()))
 
-p = None
 part1 = 0
 for i in range(len(nums) - 1):
     if nums[i+1] > nums[i]:
         part1 += 1
 print(part1)
 
-p = None
 part2 = 0
 for i in range(len(inp) - 2):
     if sum(nums[i+1:i+4]) > sum(nums[i:i+3]):
