24244dbf3d14b0456bd18a5862ec484e252f2bb9
tony chang <tony@ponderer.org>
2021-12-03 19:48:32 -0800

    Refactor day 3 to be more pythonic.

--- 2021/day03.py
+++ 2021/day03.py
@@ -18,11 +18,12 @@ inp = open(re.match(r"day\d\d", __file__)[0] + 'input.txt').read().strip()
 # 01010'''
 
 lines = inp.splitlines()
-ones = [0] * len(lines[0])
-for digits in lines:
-    for i, digit in enumerate(digits):
-        if digit == '1':
-            ones[i] += 1
+def countOnesInColumns(numbers):
+    columns = zip(*numbers)
+    ones = list(map(lambda col: col.count('1'), columns))
+    return ones
+
+ones = countOnesInColumns(lines)
 gamma = ''
 epsilon = ''
 for cnt in ones:
@@ -32,46 +33,38 @@ for cnt in ones:
     else:
         gamma += '0'
         epsilon += '1'
+part1 = int(gamma, 2) * int(epsilon, 2)
+print(part1)
 
-print(int(gamma, 2) * int(epsilon, 2))
-
-
-def find_number(lines, prefix, is_oxygen):
-    ones = 0
-    for line in lines:
-        if line[0] == '1':
-            ones += 1
+def filter_numbers(numbers, column, is_oxygen):
+    ones = countOnesInColumns(numbers)
     ret = []
-    for line in lines:
-        if is_oxygen and ones > len(lines) / 2 and line[0] == '1':
-            ret.append(line[1:])
-            n_prefix = '1'
-        elif is_oxygen and ones < len(lines) / 2 and line[0] == '0':
-            ret.append(line[1:])
-            n_prefix = '0'
-        elif is_oxygen and ones == len(lines) / 2 and line[0] == '1':
-            ret.append(line[1:])
-            n_prefix = '1'
-        elif not is_oxygen and ones > len(lines) / 2 and line[0] == '0':
-            ret.append(line[1:])
-            n_prefix = '0'
-        elif not is_oxygen and ones < len(lines) / 2 and line[0] == '1':
-            ret.append(line[1:])
-            n_prefix = '1'
-        elif not is_oxygen and ones == len(lines) / 2 and line[0] == '0':
-            ret.append(line[1:])
-            n_prefix = '0'
-    return ret, prefix + n_prefix
+    if is_oxygen:
+        for number in numbers:
+            if ((ones[column] > len(numbers) / 2 and number[column] == '1')
+                or (ones[column] < len(numbers) / 2 and number[column] == '0')
+                or (ones[column] == len(numbers) / 2 and number[column] == '1')):
+                ret.append(number)
+    else:
+        for number in numbers:
+            if ((ones[column] > len(numbers) / 2 and number[column] == '0')
+                or (ones[column] < len(numbers) / 2 and number[column] == '1')
+                or (ones[column] == len(numbers) / 2 and number[column] == '0')):
+                ret.append(number)
+    return ret
+
 
 possible = lines[:]
-prefix = ''
+column = 0
 while len(possible) > 1:
-    possible, prefix = find_number(possible, prefix, True)
-oxygen_rating = prefix + possible[0]
+    possible = filter_numbers(possible, column, is_oxygen=True)
+    column += 1
+oxygen_rating = possible[0]
 
 possible = lines[:]
-prefix = ''
+column = 0
 while len(possible) > 1:
-    possible, prefix = find_number(possible, prefix, False)
-co2_rating = prefix + possible[0]
+    possible = filter_numbers(possible, column, is_oxygen=False)
+    column += 1
+co2_rating = possible[0]
 print(int(oxygen_rating, 2) * int(co2_rating, 2))
