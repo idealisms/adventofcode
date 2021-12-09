82d8eaf8cbe98473284be9ad911f4cb1ff2903a4
tony chang <tony@ponderer.org>
2021-12-08 21:24:10 -0800

    Solutions to day 9.

---
+++ 2021/day09.py
@@ -0,0 +1,42 @@
+import collections
+import itertools
+import math
+import re
+
+inp = open(re.match(r"day\d\d", __file__)[0] + 'input.txt').read().strip()
+
+grid = inp.splitlines()
+rows = len(grid)
+cols = len(grid[0])
+grid = []
+grid.append(['9'] * (cols+2))
+for row in inp.splitlines():
+    grid.append(list('9' + row + '9'))
+grid.append(['9'] * (cols+2))
+
+part1 = 0
+for r in range(1, rows + 1):
+    for c in range(1, cols + 1):
+        if (grid[r][c] < grid[r-1][c] and grid[r][c] < grid[r][c-1]
+            and grid[r][c] < grid[r+1][c] and grid[r][c] < grid[r][c+1]):
+            part1 += int(grid[r][c]) + 1
+print(part1)
+
+def fill(grid, r, c):
+    if grid[r][c] == '9':
+        return 0
+    grid[r][c] = '9'
+    size = 1
+    size += fill(grid, r-1, c)
+    size += fill(grid, r+1, c)
+    size += fill(grid, r, c-1)
+    size += fill(grid, r, c+1)
+    return size
+
+sizes = []
+for r in range(1, rows + 1):
+    for c in range(1, cols + 1):
+        if grid[r][c] != '9':
+            sizes.append(fill(grid, r, c))
+sizes.sort()
+print(sizes[-3] * sizes[-2] * sizes[-1])
